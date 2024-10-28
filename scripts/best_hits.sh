c7b1d8c1382213d048372a49cb3f87007aae63b3
Jade <jade_leconte@hotmail.fr>
2024-10-28 15:47:52 +0100

    bug fix on best hit computation for CDS mode

--- scripts/best_hits.sh
+++ scripts/best_hits.sh
@@ -2,13 +2,13 @@
 
 split -l ${snakemake_params[CHUNKSIZE]} "${snakemake_input[0]}" "${snakemake_params[CHUNK]}"
 
-for file in "${snakemake_params[CHUNK]}"*; do Rscript ${snakemake_params[SORTPATH]} $file ${snakemake_params[SORTED]}$(basename $file); done
+for file in "${snakemake_params[CHUNK]}"*; do Rscript ${snakemake_params[SORTPATH]} $file ${snakemake_params[SORTED]}$(basename $file) ${snakemake_params[SEPARATOR]}; done
 
-sort --parallel=${snakemake_params[MMSEQS_THREADS]} -m -k1,1 -k2,2 -k7,7g "${snakemake_params[SORTED]}"* > "${snakemake_params[MERGED]}"
+sort --parallel=${snakemake_params[MMSEQS_THREADS]} -m -k1,1 -k9,9 -k7,7g "${snakemake_params[SORTED]}"* > "${snakemake_params[MERGED]}"
 
 rm "${snakemake_params[CHUNK]}"* "${snakemake_params[SORTED]}"*
 
-datamash -g 1,2 first 3 first 4 first 5 first 6 first 7 first 8 < "${snakemake_params[MERGED]}" > "${snakemake_output[0]}"
+datamash -g 1,9 first 2 first 3 first 4 first 5 first 6 first 7 first 8 < "${snakemake_params[MERGED]}" | cut -f1,3-9 > "${snakemake_output[0]}"
 
 rm "${snakemake_params[MERGED]}"
 
--- scripts/sort.R
+++ scripts/sort.R
@@ -6,12 +6,16 @@ library(data.table)
 args <- commandArgs(trailingOnly = TRUE)
 input_file <- args[1]  # Input file
 output_file <- args[2]  # Output file
+separator <- args[3]       # Separator for splitting (e.g., "_CDS")
 
 # Read the input file into a data.table
 dt <- fread(input_file)
 
+# Create a new column (V9) by splitting V2 on the specified separator
+dt[, V9 := sapply(strsplit(as.character(V2), separator), `[`, 1)]
+
 # Sort the data.table by the specified keys (adjust column names as needed)
-setorder(dt, V1, V2, V7)
+setorder(dt, V1, V9, V7)
 
 # Write the sorted data.table to the output file
 fwrite(dt, output_file, sep = "\t", col.names=F)
